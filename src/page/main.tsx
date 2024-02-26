da3c03fa6d59416a82b6f571647bfd9ef1adacf7
Aleksey Svishchev <aleksey.svischev01@gmail.com>
2024-02-26 21:07:33 +0300

    refactor: Исправил основной фон

--- src/page/main.tsx
+++ src/page/main.tsx
@@ -4,7 +4,7 @@ import TodoCard from '../widget/TodoCard'
 
 function Main() {
   return (
-    <div className='flex justify-center flex-col items-center h-screen bg-gradient-to-r from-purple-500 to-pink-500'>
+    <div className='flex justify-center flex-col items-center h-screen bg-gradient-to-r from-purple-200 to-pink-200'>
       <Routes>
         <Route path='/' element={<TodoCard />} />
         <Route path='/tasks/:id' element={<TaskCard />} />
